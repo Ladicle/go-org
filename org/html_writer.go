75a20ac6e10bc60b4f152bf937115b3fb77f8802
Aya Igarashi <ladiclexxx@gmail.com>
2022-11-26 15:25:00 +0900

    Add FIXME tag

--- org/html_writer.go
+++ org/html_writer.go
@@ -370,7 +370,7 @@ func (w *HTMLWriter) WriteRegularLink(l RegularLink) {
 	case "id":
 		file := w.document.IDLinks[l.URL]
 		url = strings.TrimPrefix(file, w.document.ContentDir)
-		url = strings.TrimPrefix(file, "/Users/ladicle/doc/content") // workaround for roam-db
+		url = strings.TrimPrefix(file, "/Users/ladicle/doc/content") // FIXME: workaround for roam-db
 		if !strings.HasPrefix(url, "/") {
 			url = "/" + url
 		}
